41fdc8cb910703c1d0baa9da37b69c60211ade33
Hernan-Valdez <hernan_valdez@msn.com>
2024-09-23 12:38:49 -0700

    Add files via upload

---
+++ nombreapellido ios/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+int main() {
+    std::string nombre, apellido;
+
+    std::cout << "Ingresa tu nombre: ";
+    std::getline(std::cin, nombre);
+
+    std::cout << "Ingresa tu apellido: ";
+    std::getline(std::cin, apellido);
+
+    std::cout << "El nombre que ingresaste es: " << nombre << " " << apellido << std::endl;
+
+    return 0;
+}
