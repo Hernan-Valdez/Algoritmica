448204f9c25d83a5481f53e5a8a88f271bfa8b09
Hernan-Valdez <hernan_valdez@msn.com>
2024-11-01 17:29:08 -0700

    do while.cpp

---
+++ Segundo Parcial/dowhile.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int numero = 10;
+
+    // Ciclo do-while que cuenta de 10 a 1
+    do {
+        cout << numero << endl;
+        numero--;
+    } while (numero >= 1);
+
+    return 0;
+}
